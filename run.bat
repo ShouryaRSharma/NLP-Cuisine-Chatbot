f05f0bcfc3a67ed82f52b6386e4912e5a17a0db7
Archie Godfrey <archiegodfrey1999@outlook.com>
2022-05-21 08:09:33 +0100

    Added images for Q4 and Q5 and ports to run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 docker build -m 2g -t training -f "TrainingDocker" . & docker run --gpus all training
-docker build -m 2g -t chatbot -f "ChatbotDocker" . & docker run --gpus all chatbot
+docker build -m 2g -t chatbot -f "ChatbotDocker" . & docker run --gpus all -p 5000:5000 chatbot
